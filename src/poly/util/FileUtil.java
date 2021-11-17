5504449087685ed6c52e860a81a0d55b37de1b0a
Syn <Daehyun2ya@gmail.com>
2021-11-17 08:00:00 +0000

    #Util

---
+++ src/poly/util/FileUtil.java
@@ -0,0 +1,26 @@
+package poly.util;
+
+import java.io.File;
+
+
+public class FileUtil {
+  
+     /**
+      * 현재 날짜를 기준으로 년/ 월 / 일 폴더 생성하기
+      * 년월일 별로 폴더를 생성하는 함수
+      * @param 파일이 저장되는 Root 폴더
+      * @return 파일이 저장되기 위해 생성된 전체 폴더 경로
+      */
+   public static String mkdirForDate(String uploadDir) {
+	   
+	   String path = uploadDir + DateUtil.getDateTime("/yyyy/MM/dd"); //폴더 경로
+	   
+	   File Folder = new File(path);
+	   
+	   if (!Folder.exists()) {
+		   Folder.mkdirs(); //폴더 생성합니다.
+		   
+	   }
+	   return path;
+   }
+}
